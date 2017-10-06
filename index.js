c134c7fb2803020e9cab986b5664e34d52d0f478
Elliot Guzman <guleca87@gmail.com>
2017-10-06 18:37:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const app = "I don't do much."
 
 var kittens = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendKitten(name) {
-  kittens.push["Ralph"]
-}
-
 function destructivelyPrependKitten(name) {
   kittens.unshift(name)
 } 
