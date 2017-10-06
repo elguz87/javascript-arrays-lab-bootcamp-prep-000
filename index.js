ec5b6b0ba204e1423a7f1728a827371d96826ae4
Elliot Guzman <guleca87@gmail.com>
2017-10-06 18:33:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const app = "I don't do much."
 
 var kittens = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendKitten(name){
-  kittens.push['Ralph']
+function destructivelyAppendKitten(name) {
+  kittens.push[name]
 }
